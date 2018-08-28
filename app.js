9724ddd6298f09fe22cf08d1f063b08a2e5812b4
Ryan McCrory <ryanmccrory83@gmail.com>
2018-08-28 13:15:54 -0600

    corrected package.json issue

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const cors =require('cors');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 8000;
 const queries = require('./queries.js');
---
+++ seeds/README.md
@@ -0,0 +1,5 @@
+This application is full CRUD capable.  It uses heroku in combination with VUE.js to populate books through Vue router.
+
+Take a tour of the website to see what books I have added to my database.
+
+Heroku Deployed Link
